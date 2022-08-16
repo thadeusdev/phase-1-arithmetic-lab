eb1adda8c0431f4438ffccb5d4c076b7e4957dbe
thadeus <thadeusnyariki94@gmail.com>
2022-08-16 18:23:48 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
 // Write your code here
+let num1 = 7;
+let num2 = 9;
+
+let multiply = num1 * num2 - 1
+
+let random = Math.floor(Math.random()) + 1;
+
+let num3 = 25;
+let num4 = 7;
+let mod = num3 % num4;
+
+let max = Math.max(20);
